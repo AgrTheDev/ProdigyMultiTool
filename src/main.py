d3b9dc3c4ea555a328cb54ac0e6d41a3533b729d
AGR <63412264+AGR-DEVS@users.noreply.github.com>
2022-03-29 21:32:17 -0400

    additions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,33 @@
-import time
+import time, os
+from colorama import Fore
 
-print('hello sus')
+
+# Login implementation (save username, password, authentication token (created_at, expires_in) to settings.json)
+
+# Menu system (display menu, get user input, call function)
+
+os.system("cls")
+
+print(f"""{Fore.GREEN}
+  ____                _ _                 __  __       _____ 
+ |  _ \ _ __ ___   __| (_) __ _ _   _    |  \/  |     |_   _|
+ | |_) | '__/ _ \ / _` | |/ _` | | | |   | |\/| |       | |  
+ |  __/| | | (_) | (_| | | (_| | |_| |   | |  | |  _    | |  
+ |_|   |_|  \___/ \__,_|_|\__, |\__, |   |_|  |_| (_)   |_|  
+                          |___/ |___/                        
+{Fore.RESET}""")
+
+print(f"{Fore.BLUE}~{Fore.RESET}" * 60)
+print(f"""
+(1) - Player Hacks
+(2) - Logout
+{Fore.CYAN}
+╔════════════════════════╗
+  Logged in as: BuddyR11
+╚════════════════════════╝{Fore.RESET}
+""")
+print(f"{Fore.BLUE}~{Fore.RESET}" * 60)
+
+input(">>> ")
+
 time.sleep(5)
